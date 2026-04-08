56b30f814fc04da6182df37348bfbda117129a39
UnderHear <2533643340@qq.com>
2026-04-09 00:06:29 +0800

    更换Noto Sans SC字体

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
+import "@fontsource-variable/noto-sans-sc";
 import App from "./App";
 import { AppToaster } from "./components/AppToaster";
 import { SettingsProvider } from "./lib/settings";
